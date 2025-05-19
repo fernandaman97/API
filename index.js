fa142c5935ae995f3d6e550cd93e274552dab327
fernandaman97 <fernandamantilla2016@gmail.com>
2025-05-19 15:04:05 -0500

    modifiqué el index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express= require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan= require('morgan');
 
-app.get('/', (req, res)=>{
-    res.send('Servidor Funcionando');
-});
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.json())
+
+const postRoute=require('./routes/post')
+
+app.use('./servicios',postRoute);
+
+mongoose.connect('mongodb+srv://Cluster0:0nXxBBanGlPnSzla@cluster1.sfuh05w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 
 app.listen(10000);
